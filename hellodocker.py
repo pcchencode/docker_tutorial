5e0bcd54f274f13dd1e4961ca3b8f310f1bd6a13
pcchencode <37117357+pcchencode@users.noreply.github.com>
2020-03-05 21:02:47 +0800

    Add files via upload

---
+++ hellodocker.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def main():
+	lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
+	data = pd.DataFrame(lst)
+	data.to_csv('data.csv')
+    
+	return
+	#print(get_href_from_page(board_name=str(Board_Name), scrap_page=int(Scrap_Page)))
+
+
+if __name__ == '__main__':
+
+	main()
+
