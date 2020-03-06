856fc250eb79ffa758f2f104ad0b9de41d7187b0
pcchencode <37117357+pcchencode@users.noreply.github.com>
2020-03-06 14:45:28 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,6 @@ FROM python:3.7.3-stretch
 
 ENV TZ Asia/Taipei
 
-
-
 COPY ./requirements.txt /requirements.txt
-COPY ./hellodocker.py /hellodocker.py
 RUN pip install -r /requirements.txt
 
-CMD ["python3","hellodocker.py"]
--- hellodocker.py
+++ hellodocker.py
@@ -4,8 +4,8 @@ import pandas as pd
 def main():
 	lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
 	data = pd.DataFrame(lst)
-	data.to_csv('data.csv')
-    
+	data.to_csv('/docker_test/data.csv')
+	print("file saved.")
 	return
 	#print(get_href_from_page(board_name=str(Board_Name), scrap_page=int(Scrap_Page)))
 
