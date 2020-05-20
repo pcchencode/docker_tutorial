11378f66d41fc91c3d1769225dc15b5347bc7b4e
POCHU CHEN <jerry.chen@langlive.com>
2020-05-20 23:53:23 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM python:3.7.3-stretch
 
 ENV TZ Asia/Taipei
 
-COPY ./requirements.txt /requirements.txt
+COPY requirements.txt .
+COPY hellodocker.py .
 RUN pip install -r /requirements.txt
 
 CMD ["python3","hellodocker.py"]
--- hellodocker.py
+++ hellodocker.py
@@ -4,9 +4,10 @@ import pandas as pd
 def main():
 	lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
 	data = pd.DataFrame(lst)
-	#data.to_csv('/data.csv')
-	#print("file saved.")
+	data.to_csv('/tmp/data.csv')
+	print("file saved.")
 	print(data)
+	print('hello')
 	return
 	#print(get_href_from_page(board_name=str(Board_Name), scrap_page=int(Scrap_Page)))
 
